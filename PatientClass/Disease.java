84807ae930d2fe44d5eb39cae4b24de725d3e453
Aditya <aditya.murthy@asu.edu>
2016-09-29 00:57:58 -0700

    Disease class

---
+++ PatientClass/Disease.java
@@ -0,0 +1,13 @@
+
+public class Disease {
+	private String diseaseName;
+
+	public String getDiseaseName() {
+		return diseaseName;
+	}
+
+	public boolean setDiseaseName(String diseaseName) {
+		this.diseaseName = diseaseName;
+		return false;
+	}
+}
